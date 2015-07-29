a13bcb6ccad771d3a0bef7931db01ba2e85036a2
carlosp420 <mycalesis@gmail.com>
2015-07-29 16:15:46 +0300

    standard table

---
+++ degenerate_dna/tables.py
@@ -0,0 +1,317 @@
+"""Degenerated tables go here.
+"""
+degenerated_table_1 = {
+    # Arginine1, Arginine2 & Serine2
+    'CGA': 'MGN',
+    'CGC': 'MGN',
+    'CGT': 'MGN',
+    'CGG': 'MGN',
+    'CGM': 'MGN',
+    'CGW': 'MGN',
+    'CGR': 'MGN',
+    'CGY': 'MGN',
+    'CGS': 'MGN',
+    'CGK': 'MGN',
+    'CGH': 'MGN',
+    'CGV': 'MGN',
+    'CGD': 'MGN',
+    'CGB': 'MGN',
+    'CGN': 'MGN',
+    'AGA': 'MGN',
+    'AGG': 'MGN',
+    'AGR': 'MGN',
+    'AGM': 'MGN',
+    'AGW': 'MGN',
+    'AGS': 'MGN',
+    'AGK': 'MGN',
+    'AGH': 'MGN',
+    'AGV': 'MGN',
+    'AGD': 'MGN',
+    'AGB': 'MGN',
+    'AGN': 'MGN',
+    'MGA': 'MGN',
+    'MGC': 'MGN',
+    'MGT': 'MGN',
+    'MGG': 'MGN',
+    'MGM': 'MGN',
+    'MGW': 'MGN',
+    'MGR': 'MGN',
+    'MGY': 'MGN',
+    'MGS': 'MGN',
+    'MGK': 'MGN',
+    'MGH': 'MGN',
+    'MGV': 'MGN',
+    'MGD': 'MGN',
+    'MGB': 'MGN',
+    'MGN': 'MGN',
+
+    # Phenylalanine
+    'TTT': 'TTY',
+    'TTC': 'TTY',
+    'TTY': 'TTY',
+
+    # Leucine1, Phenylalanine & Leucine2
+    'TTA': 'YTN',
+    'TTG': 'YTN',
+    'TTM': 'YTN',
+    'TTW': 'YTN',
+    'TTR': 'YTN',
+    'TTS': 'YTN',
+    'TTK': 'YTN',
+    'TTH': 'YTN',
+    'TTV': 'YTN',
+    'TTD': 'YTN',
+    'TTB': 'YTN',
+    'TTN': 'YTN',
+
+    'CTA': 'YTN',
+    'CTC': 'YTN',
+    'CTG': 'YTN',
+    'CTT': 'YTN',
+    'CTM': 'YTN',
+    'CTW': 'YTN',
+    'CTR': 'YTN',
+    'CTY': 'YTN',
+    'CTS': 'YTN',
+    'CTK': 'YTN',
+    'CTH': 'YTN',
+    'CTV': 'YTN',
+    'CTD': 'YTN',
+    'CTB': 'YTN',
+    'CTN': 'YTN',
+
+    'YTA': 'YTN',
+    'YTC': 'YTN',
+    'YTT': 'YTN',
+    'YTG': 'YTN',
+    'YTM': 'YTN',
+    'YTW': 'YTN',
+    'YTR': 'YTN',
+    'YTY': 'YTN',
+    'YTS': 'YTN',
+    'YTK': 'YTN',
+    'YTH': 'YTN',
+    'YTV': 'YTN',
+    'YTD': 'YTN',
+    'YTB': 'YTN',
+    'YTN': 'YTN',
+
+    # Isoleucine
+    'ATA': 'ATH',
+    'ATC': 'ATH',
+    'ATT': 'ATH',
+    'ATM': 'ATH',
+    'ATW': 'ATH',
+    'ATY': 'ATH',
+    'ATH': 'ATH',
+
+    # Methionine
+    'ATG': 'ATG',
+
+    # Isoleucine & Methionine
+    'ATR': 'ATN',
+    'ATS': 'ATN',
+    'ATK': 'ATN',
+    'ATV': 'ATN',
+    'ATD': 'ATN',
+    'ATB': 'ATN',
+    'ATN': 'ATN',
+
+    # Valine
+    'GTA': 'GTN',
+    'GTC': 'GTN',
+    'GTT': 'GTN',
+    'GTG': 'GTN',
+    'GTM': 'GTN',
+    'GTW': 'GTN',
+    'GTR': 'GTN',
+    'GTY': 'GTN',
+    'GTS': 'GTN',
+    'GTK': 'GTN',
+    'GTH': 'GTN',
+    'GTV': 'GTN',
+    'GTD': 'GTN',
+    'GTB': 'GTN',
+    'GTN': 'GTN',
+
+    # Serine1
+    'TCA': 'TCN',
+    'TCC': 'TCN',
+    'TCT': 'TCN',
+    'TCG': 'TCN',
+    'TCM': 'TCN',
+    'TCW': 'TCN',
+    'TCR': 'TCN',
+    'TCY': 'TCN',
+    'TCS': 'TCN',
+    'TCK': 'TCN',
+    'TCH': 'TCN',
+    'TCV': 'TCN',
+    'TCD': 'TCN',
+    'TCB': 'TCN',
+    'TCN': 'TCN',
+
+    # Serine2
+    'AGT': 'AGY',
+    'AGC': 'AGY',
+    'AGY': 'AGY',
+
+    # Proline
+    'CCA': 'CCN',
+    'CCC': 'CCN',
+    'CCT': 'CCN',
+    'CCG': 'CCN',
+    'CCM': 'CCN',
+    'CCW': 'CCN',
+    'CCR': 'CCN',
+    'CCY': 'CCN',
+    'CCS': 'CCN',
+    'CCK': 'CCN',
+    'CCH': 'CCN',
+    'CCV': 'CCN',
+    'CCD': 'CCN',
+    'CCB': 'CCN',
+    'CCN': 'CCN',
+
+    # Threonine
+    'ACA': 'ACN',
+    'ACC': 'ACN',
+    'ACT': 'ACN',
+    'ACG': 'ACN',
+    'ACM': 'ACN',
+    'ACW': 'ACN',
+    'ACR': 'ACN',
+    'ACY': 'ACN',
+    'ACS': 'ACN',
+    'ACK': 'ACN',
+    'ACH': 'ACN',
+    'ACV': 'ACN',
+    'ACD': 'ACN',
+    'ACB': 'ACN',
+    'ACN': 'ACN',
+
+    # Alanine
+    'GCA': 'GCN',
+    'GCC': 'GCN',
+    'GCT': 'GCN',
+    'GCG': 'GCN',
+    'GCM': 'GCN',
+    'GCW': 'GCN',
+    'GCR': 'GCN',
+    'GCY': 'GCN',
+    'GCS': 'GCN',
+    'GCK': 'GCN',
+    'GCH': 'GCN',
+    'GCV': 'GCN',
+    'GCD': 'GCN',
+    'GCB': 'GCN',
+    'GCN': 'GCN',
+
+    # Tyrosine
+    'TAT': 'TAY',
+    'TAC': 'TAY',
+    'TAY': 'TAY',
+
+    # Histidine
+    'CAT': 'CAY',
+    'CAC': 'CAY',
+    'CAY': 'CAY',
+
+    # Glutamine
+    'CAA': 'CAR',
+    'CAG': 'CAR',
+    'CAR': 'CAR',
+
+    # Histidine & Glutamine
+    'CAM': 'CAN',
+    'CAW': 'CAN',
+    'CAS': 'CAN',
+    'CAK': 'CAN',
+    'CAH': 'CAN',
+    'CAV': 'CAN',
+    'CAD': 'CAN',
+    'CAB': 'CAN',
+    'CAN': 'CAN',
+
+    # Asparagine
+    'AAT': 'AAY',
+    'AAC': 'AAY',
+    'AAY': 'AAY',
+
+    # Lysine
+    'AAA': 'AAR',
+    'AAG': 'AAR',
+    'AAR': 'AAR',
+
+    # Asparagine & Lysine
+    'AAM': 'AAN',
+    'AAW': 'AAN',
+    'AAS': 'AAN',
+    'AAK': 'AAN',
+    'AAH': 'AAN',
+    'AAV': 'AAN',
+    'AAD': 'AAN',
+    'AAB': 'AAN',
+    'AAN': 'AAN',
+
+    # Aspartic acid
+    'GAT': 'GAY',
+    'GAC': 'GAY',
+    'GAY': 'GAY',
+
+    # Glutamic acid
+    'GAA': 'GAR',
+    'GAG': 'GAR',
+    'GAR': 'GAR',
+
+    # Aspartic acid & Glutamic acid
+    'GAM': 'GAN',
+    'GAW': 'GAN',
+    'GAS': 'GAN',
+    'GAK': 'GAN',
+    'GAH': 'GAN',
+    'GAV': 'GAN',
+    'GAD': 'GAN',
+    'GAB': 'GAN',
+    'GAN': 'GAN',
+
+    # Cysteine
+    'TGT': 'TGY',
+    'TGC': 'TGY',
+    'TGY': 'TGY',
+
+    # Tryptophan
+    'TGG': 'TGG',
+
+    # Cysteine & Tryptophan
+    'TGK': 'TGB',
+    'TGS': 'TGB',
+    'TGB': 'TGB',
+
+    # Glycine
+    'GGA': 'GGN',
+    'GGC': 'GGN',
+    'GGT': 'GGN',
+    'GGG': 'GGN',
+
+    'GGM': 'GGN',
+    'GGW': 'GGN',
+    'GGR': 'GGN',
+    'GGY': 'GGN',
+    'GGS': 'GGN',
+    'GGK': 'GGN',
+
+    'GGH': 'GGN',
+    'GGV': 'GGN',
+    'GGD': 'GGN',
+    'GGB': 'GGN',
+
+    'GGN': 'GGN',
+    'GGX': 'GGN',
+
+    # degen
+    'NNNN': 'NNN',
+
+    # indel
+    '---': '---',
+}
